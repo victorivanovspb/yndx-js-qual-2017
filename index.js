d1777b98d55c348dff9e6cce40b7e4388d03636a
Victor Ivanov <victorivanov.spb@gmail.com>
2017-08-29 23:19:53 +0300

    Upd class Validator (index.js)

--- index.js
+++ index.js
@@ -1,8 +1,54 @@
 /**
- * Клиентская логика
+ * Выполняется механизм валидации передаваемых строк.
+ * Предварительно загружаются (метод addRegexp) проверочные регулярные выражения и вспомогательные функции.
  */
 class Validator {
-    constructor() {}
+    constructor() {
+        this.regs = [];
+    }
+
+    /**
+     * Выполняется загрузка регулярного выражения для проверки и вспомогательных функций.
+     * @param item - идентификатор, по которому запрашивается соотв. набор регулярного выражения и функций.
+     * @param regexp - регулярное выражение.
+     * @param prepareFunc - функция для предварительной подготовки сторки перед сравнением с рег. выражением.
+     *      Реализована функция по умолчанию, выполняющая обрезку пустых символов по краям строки.
+     * @param afterFunc - функция для выполнения окончательной проверки после сравнения с рег. выражением.
+     *      Реализована функция по умолчанию, возвращающая подвтерждение проверки, если строка не равна null.
+     * @returns {Validator} - ссылка на объект Validator.
+     */
+    addRegexp(item, regexp, prepareFunc=function(str) { return str.trim(); }, afterFunc=function(str) { return str !== null; }) {
+        this.regs[item] = {
+            regexp: regexp,
+            prepare: prepareFunc,
+            after: afterFunc
+        };
+        return this;
+    }
+
+    /**
+     * @param data - данные, передаваемые для валидации
+     * @param item - идентификатор, по которому запрашивается соотв. набор регулярного выражения и функций.
+     * @returns {result/null} - строка, прошедшая валидацию, либо null.
+     */
+    validate(data, item) {
+        let result = null;
+        let regexp = this.regs[item].regexp;
+        if (regexp !== undefined) {
+            try {
+                result = this.regs[item]
+                    .prepare(data)
+                    .match(regexp);
+                if (result !== null && this.regs[item].after(result[0]) === false) {
+                    result = null;
+                }
+            } catch (e) {
+                console.log("Validator throws exception: " + e.message);
+                result = null;
+            }
+        }
+        return result !== null ? result[0] : null;
+    }
 }
 
 /**
