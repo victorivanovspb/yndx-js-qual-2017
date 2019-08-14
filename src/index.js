2467307a9683e8621e0230cb3308d8622b33d353
Victor <victorivanov.spb@gmail.com>
2019-08-14 17:16:12 +0300

    Refactor project
    Upd package.json. Upd gitignore. Add webpack. Add bootstrap. Refactor src

---
+++ src/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import './css/styles.scss';
+import './bootstrap/bootstrap.js'
+
+jQuery(document)
+    .ready(function($) {
+        console.log('jquery');
+
+        /*
+        ((selector) => {
+            const color = (Math.random() > 0.5) ? '#eee' : '#fff';
+            selector.css('background-color', color);
+        })( $('body') );
+        */
+    });
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+        new CleanWebpackPlugin({
+            path: './dist/*.*'
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jquery': 'jquery'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    }
+};
